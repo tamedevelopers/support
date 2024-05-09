8b8bf4616f9f85d7cadfe8ece1ab80b62e71144e
Fredrick Peter <tamedevelopers@gmail.com>
2024-05-09 05:24:57 +0100

    update

--- Str.php
+++ Str.php
@@ -928,12 +928,12 @@ class Str
     /**
      * Convert the case of a string based on the specified type.
      *
-     * @param string|null $string The input string
-     * @param string|null $type   The case to convert: 'lower', 'upper', or 'unchanged'
+     * @param mixed $string The input string
+     * @param string|int|null $type   The case to convert: 'lower', 'upper', or 'unchanged'
      *
      * @return string The string with converted case
      */
-    private static function convertCase(string|null $string, string|null $type)
+    private static function convertCase($string = null, string|null $type)
     {
         return match (self::lower($type)) {
             'upper', 'uppercase', 'upper_case' => self::upper($string),
