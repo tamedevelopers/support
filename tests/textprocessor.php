1b52ff15d8179b74a1e8b29813b2cf6f7dc8c5f5
Fredrick Peter <tamedevelopers@gmail.com>
2025-09-12 01:02:17 +0100

    TextProcessor

--- tests/textprocessor.php
+++ tests/textprocessor.php
@@ -16,6 +16,7 @@ I love #coding with @friends. I also have two cats. 11092092-3";
 
 $text2 = "I'm a beautiful zero nine zero and sweet in seven 
     five seven eight available from ";
+    
 $text3 = "Reach me on zero eight one double two 555 nine or 0812225559";
 
 dd(
