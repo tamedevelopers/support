fd350f12913c077bbe667045beda0405fabe6ad6
Fredrick Peter <tamedevelopers@gmail.com>
2025-09-16 07:57:52 +0100

    update

--- src/stubs/emailProviders.php
+++ src/stubs/emailProviders.php
@@ -15,18 +15,34 @@ return [
 
     "gmail.com"         => ["strip_plus" => true, "strip_dots" => false],
     "googlemail.com"    => ["strip_plus" => true, "strip_dots" => false],
+
     "outlook.com"       => ["strip_plus" => true, "strip_dots" => false],
     "hotmail.com"       => ["strip_plus" => true, "strip_dots" => false],
     "live.com"          => ["strip_plus" => true, "strip_dots" => false],
     "msn.com"           => ["strip_plus" => true, "strip_dots" => false],
+
     "icloud.com"        => ["strip_plus" => true, "strip_dots" => false],
     "me.com"            => ["strip_plus" => true, "strip_dots" => false],
     "mac.com"           => ["strip_plus" => true, "strip_dots" => false],
+
     "fastmail.com"      => ["strip_plus" => true, "strip_dots" => false],
+
     "protonmail.com"    => ["strip_plus" => true, "strip_dots" => false],
-    "yahoo.com"         => ["strip_plus" => true, "strip_dots" => false],
-    "ymail.com"         => ["strip_plus" => true, "strip_dots" => false],
-    "rocketmail.com"    => ["strip_plus" => true, "strip_dots" => false],
+    "proton.me"         => ["strip_plus" => true, "strip_dots" => false],
+    "pm.me"             => ["strip_plus" => true, "strip_dots" => false],
+
     "zoho.com"          => ["strip_plus" => true, "strip_dots" => false],
-    "zohomail.com"      => ["strip_plus" => true, "strip_dots" => false]
+    "zohomail.com"      => ["strip_plus" => true, "strip_dots" => false],
+
+    "yandex.com"        => ["strip_plus" => true, "strip_dots" => false],
+    "yandex.ru"         => ["strip_plus" => true, "strip_dots" => false],
+
+    "gmx.com"           => ["strip_plus" => true, "strip_dots" => false],
+    "gmx.de"            => ["strip_plus" => true, "strip_dots" => false],
+    "mail.com"          => ["strip_plus" => true, "strip_dots" => false],
+
+    "mailbox.org"       => ["strip_plus" => true, "strip_dots" => false],
+    "posteo.de"         => ["strip_plus" => true, "strip_dots" => false],
+    "runbox.com"        => ["strip_plus" => true, "strip_dots" => false],
+    "startmail.com"     => ["strip_plus" => true, "strip_dots" => false]
 ];
