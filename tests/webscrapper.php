d51c5661666dbb8f66f68a4ffcb2f25118863816
Fredrick Peter <tamedevelopers@gmail.com>
2026-07-11 22:25:22 +0100

    update

--- tests/webscrapper.php
+++ tests/webscrapper.php
@@ -8,11 +8,11 @@ $jumia = 'https://www.jumia.com.ng/20000-mah-utra-slim-portable-power-bank-ace-e
 $jumia2 = 'https://www.jumia.com.ng/ashion-kq-mens-stylish-grey-silver-turf-football-shoes-durable-training-sports-footwear-for-match-casual-419139185.html';
 $ebay = 'https://www.ebay.com/itm/386936766515';
 $aliexpress = 'https://www.aliexpress.com/item/1005010595606666.html';
-
+$amazon = 'https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/';
 
 dd(
     WebScraper::url("
-            $aliexpress
+            $amazon
         ")
         ->setSelectors([
             // 'title' => 'h1',
